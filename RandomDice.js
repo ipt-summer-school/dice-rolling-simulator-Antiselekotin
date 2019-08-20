7e3ba72daeffc7ceb1e13dffca2a4319985de4ed
Antiselekotin <deksazon8@gmail.com>
2019-08-20 23:04:32 +0300

    Доделал задание согласно условию, МАУ

--- RandomDice.js
+++ RandomDice.js
@@ -1,2 +1,11 @@
-let i = Math.round(Math.random() * 5 + 1);
-console.log(i);
+
+function rolligDices (x){
+    let x = prompt("Please, write down quantity of rollings") 
+    let i = Math.round(Math.random() * 5 + 1);
+    console.log((x*i));
+}
+rolligDices();
+
+// Эта функция работает в связке с браузером,
+// по-этому, для роботы в консоли,
+// замените prompt на любое числовое значения.
